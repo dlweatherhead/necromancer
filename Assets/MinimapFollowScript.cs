8f87f2148f748e3d3012761f903b70bf561234cd
David W <dlweatherhead@gmail.com>
2017-11-07 21:08:02 +0200

    Cleaned up Scripts

---
+++ Assets/MinimapFollowScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapFollowScript : MonoBehaviour {
+
+	public Transform target;
+	
+	void Update () {
+		var newPos = new Vector3(target.position.x, transform.position.y, target.position.z);
+		transform.position = newPos;
+	}
+}
